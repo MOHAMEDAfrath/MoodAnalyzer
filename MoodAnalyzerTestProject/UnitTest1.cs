7de091192804da2868b342c891dda204c10cddf2
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-16 11:32:02 +0530

    [Afrath] Updated the file with Reflections

--- MoodAnalyzerTestProject/UnitTest1.cs
+++ MoodAnalyzerTestProject/UnitTest1.cs
@@ -6,65 +6,78 @@ namespace MoodAnalyzerTestProject
     [TestClass]
     public class UnitTest1
     {
-        MoodAnalyzerProgram moodSad;
-        MoodAnalyzerProgram moodHappy;
-        MoodAnalyzerProgram moodNull;
-        MoodAnalyzerProgram moodEmpty;
-        [TestInitialize]
-        public void SetUp()
-        {
-            string message = "I am in any Mood";
-            string[] moodMessage = message.Split(" ");
-            moodHappy = new MoodAnalyzerProgram(moodMessage[3]);
-            moodMessage[3] = "Sad";
-            moodSad = new MoodAnalyzerProgram(moodMessage[3]);
-            moodNull = new MoodAnalyzerProgram(null);
-            string empty = "";
-            moodEmpty = new MoodAnalyzerProgram(empty);
-            
-        }
         [TestMethod]
         public void TestMethodForSad()
         {
+            MoodAnalyzerProgram moodSad = new MoodAnalyzerProgram("Sad");
             string expected = "Sad";
             string actual = moodSad.MoodMessage();
             Assert.AreEqual(expected, actual);
 
         }
+        //Negative test Method to print sad mood
+        [TestMethod]
+        public void NegativeTestMethodForSad()
+        {
+            MoodAnalyzerProgram moodSad = new MoodAnalyzerProgram("Happy");
+            string expected = "Sad";
+            string actual = moodSad.MoodMessage();
+            Assert.AreEqual(expected, actual);
+
+        }
+        //positive test Method to print Happy mood
         [TestMethod]
         public void TestMethodForHappy()
         {
+            MoodAnalyzerProgram moodHappy = new MoodAnalyzerProgram("Happy");
             string expected = "Happy";
             string actual = moodHappy.MoodMessage();
             Assert.AreEqual(expected, actual);
         }
+        //Negative test Method to print Happy mood
+        [TestMethod]
+        public void NegativeTestMethodForHappy()
+        {
+            MoodAnalyzerProgram moodHappy = new MoodAnalyzerProgram("Sad");
+            string expected = "Happy";
+            string actual = moodHappy.MoodMessage();
+            Assert.AreEqual(expected, actual);
+        }
+        //Test method for null message
         [TestMethod]
         public void TestMethodForNull()
         {
+            MoodAnalyzerProgram moodNull = new MoodAnalyzerProgram(null);
             try
-            { 
-                 moodNull.MoodMessage();
-               
-            }catch(MoodAnalyzerCustomException ex)
+            {
+                moodNull.MoodMessage();
+
+            }
+            catch (MoodAnalyzerCustomException ex)
             {
                 string expected = "Mood cannot be null";
                 Assert.AreEqual(expected, ex.Message);
             }
         }
+        //test message for Empty message
         [TestMethod]
         public void TestMethodForEmpty()
         {
+            string empty = "";
+            MoodAnalyzerProgram moodEmpty = new MoodAnalyzerProgram(empty);
             try
             {
                 string actual = moodEmpty.MoodMessage();
 
-            }catch(MoodAnalyzerCustomException ex)
+            }
+            catch (MoodAnalyzerCustomException ex)
             {
                 string expected = "Mood has empty Message";
-                Assert.AreEqual(expected,ex.Message);
+                Assert.AreEqual(expected, ex.Message);
             }
 
         }
+        //Test methhod to create object
         [TestMethod]
         public void TestCreateObjectWithRelections()
         {
@@ -73,6 +86,7 @@ namespace MoodAnalyzerTestProject
             expected.Equals(actual);
 
         }
+        //Negative test case for no class found
         [TestMethod]
         public void NegativeTestCreateObjectWithRelections()
         {
@@ -81,6 +95,14 @@ namespace MoodAnalyzerTestProject
             expected.Equals(actual);
 
         }
+        //Negative test case for no constructor found
+        [TestMethod]
+        public void NegativeTestCreateObjectWithRelections1()
+        {
+            object expected = new MoodAnalyzerProgram();
+            object actual = MoodAnalyzerFactory.CreateObjectForMoodAnalyse("MoodAnalyzer.MoodAnalyzerProgram", "MoodAnalyzerProgra");
+            expected.Equals(actual);
 
+        }
     }
 }
